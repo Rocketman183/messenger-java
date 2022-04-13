0a97ffddc7aaf3212a48fee1d08b587046694a63
Dimoffmart <dimoffmart@gmail.com>
2022-04-13 18:52:20 +0500

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+public class AppFX extends Application {
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        pivarySatge.show();
+    }
+}
